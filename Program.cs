59569c6defc661bb7d7d88d5c1781b3d7eda5a34
VITU00777 <vicda.smp06@gmail.com>
2021-09-26 12:12:25 -0300

    AlunosAMS

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayColetion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               string[] alunosams= new string[5]{
+               
+               "Dani",
+               "Maju",
+               "Bruno",
+               "Ramon",
+               "Sofi"
+            };
+           Console.WriteLine(alunosams[2]);
+
+            for (int i = 0; i < alunosams.Length ; i++)
+            {
+                Console.WriteLine($" Os alunos apresentados são: {alunosams[i]}");
+            }
+        }
+    }
+}
